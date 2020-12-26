835e7389e88ac6642adca72d825b410708b0016a
Alberto Moura <56458241+AlberttMoura@users.noreply.github.com>
2020-12-26 17:58:07 -0300

    Adicionando a página de cadastro e recebendo dados do usuário

--- main/views.py
+++ main/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+import requests
+from . import models
 
 def home(request):
-    return render(request, 'index.html')
+    user = request.POST
+
+    password = None
+    if user['password1'] == user['password2']:
+        password = user['password1']
+
+    user_data = {
+        'name': user['name'],
+        'email': user['email'],
+        'password': password,
+    }
+    print(user_data)
+    return render(request, 'index.html', user_data)
+
+def signup(request):
+    return render(request, 'signup.html')
+
