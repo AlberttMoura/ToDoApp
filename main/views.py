6d2dfbcc1cd1eb56b487cced8e4d4ea498785981
Alberto Moura <56458241+AlberttMoura@users.noreply.github.com>
2020-12-26 23:00:03 -0300

    Tratando os dados do usuário e armazenando no BD. Melhorias na tela de cadastro.

--- main/views.py
+++ main/views.py
@@ -3,20 +3,30 @@ import requests
 from . import models
 
 def home(request):
-    user = request.POST
+    user_data = {}
+    
+    if request.POST:
+        user = request.POST
+        password = None
 
-    password = None
-    if user['password1'] == user['password2']:
-        password = user['password1']
+        if models.User.objects.filter(user_email=user['email'].upper()).exists():
+            print("Esse e-mail já foi cadastrado")
+        else:
+            if user['password1'] == user['password2']:
+                password = user['password1']
 
-    user_data = {
-        'name': user['name'],
-        'email': user['email'],
-        'password': password,
-    }
-    print(user_data)
+            user_data = {
+                'name': user['name'],
+                'email': user['email'].upper(),
+                'password': password,
+            }
+            print(f"nome:{user_data['name']}\ne-mail:{user_data['email']}\nsenha:{user_data['password']}")
+            models.User.objects.create(user_name=user_data['name'], user_email=user_data['email'], user_password=user_data['password'])
+
+
+
+
     return render(request, 'index.html', user_data)
 
 def signup(request):
     return render(request, 'signup.html')
-
---
+++ static/scripts/signup.js
@@ -0,0 +1,3 @@
+function cadastro() {
+    window.alert("oi")
+}
