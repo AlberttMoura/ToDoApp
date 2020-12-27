d15376ee1371ca747587e768029875c9b8ab274e
Alberto Moura <56458241+AlberttMoura@users.noreply.github.com>
2020-12-27 13:51:00 -0300

    Verificando os dados digitados pele usuário. Adicionando JS à página de cadastro.

--- static/scripts/signup.js
+++ static/scripts/signup.js
@@ -1,3 +1,49 @@
-function cadastro() {
-    window.alert("oi")
-}
+const x = document.getElementById('signup')
+var inputs = document.getElementsByTagName('input')
+
+x.addEventListener('click', (event)=>{
+    
+    for(let i = 1; i < inputs.length - 1; i++){
+
+        //Verificando se os campos estão preenchidos
+        if(!inputs[i].value){
+            document.getElementById('required-field' + [i]).className = 'warning-on'
+            event.preventDefault()
+        }
+
+        else{
+            document.getElementById('required-field' + [i]).className = 'warning-off'
+
+            //Verificando e-mail
+            if(inputs[i].name == 'email'){
+                if(!inputs[i].value.includes("@")){
+                    document.getElementById('invalid-email').className = 'warning-on'
+                    event.preventDefault()
+                }
+                else{
+                    if(inputs[i].value.slice(inputs[i].value.length-4).toLocaleLowerCase() != ".com" || inputs[i].value[inputs[i].value.length-5] == '@'){
+                        document.getElementById('invalid-email').className = 'warning-on'
+                        event.preventDefault()
+                    }
+                    else{
+                        document.getElementById('invalid-email').className = 'warning-off'
+                    }
+
+                }
+            }
+            //Verificando senhas
+            else if(inputs[i].name.toLocaleLowerCase() == "password2") {
+                if(inputs[i].value != inputs[i-1].value) {
+                    document.getElementById('passwords-dont-match').className = 'warning-on'
+                    event.preventDefault()
+                }
+                else{
+                    document.getElementById('passwords-dont-match').className = 'warning-off'
+                }
+            }
+
+        }
+
+    }
+    
+})
